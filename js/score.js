e058253e74581084e0cf1da2760cc62913873c01
Trpii-1 <trpii0311@gmail.com>
2026-07-18 22:10:14 +0100

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,13 +24,13 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 350) *
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 215) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
 
     if (percent != 100) {
-        return round(score - score / 8);
+        return round(score - score / 3);
     }
 
     return Math.max(round(score), 0);
